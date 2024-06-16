f2a900846418d4915d77c7c962919466104d3cef
Alexander <alex@alexvoiceover.com>
2024-06-16 21:46:25 +0100

    Challenge 3

--- src/routes/log-in.js
+++ src/routes/log-in.js
@@ -1,9 +1,11 @@
-const { getUserByEmail } = require("../model/user.js");
-const { Layout } = require("../templates.js");
+const { getUserByEmail } = require('../model/user.js')
+const { Layout } = require('../templates.js')
+const  bcrypt  = require('bcryptjs')
+const { createSession } = require('../model/session')
 
 function get(req, res) {
-  const title = "Log in to your account";
-  const content = /*html*/ `
+	const title = 'Log in to your account'
+	const content = /*html*/ `
     <div class="Cover">
       <h1>${title}</h1>
       <form method="POST" class="Row">
@@ -18,25 +20,40 @@ function get(req, res) {
         <button class="Button">Log in</button>
       </form>
     </div>
-  `;
-  const body = Layout({ title, content });
-  res.send(body);
+  `
+	const body = Layout({ title, content })
+	res.send(body)
 }
 
-function post(req, res) {
-  const { email, password } = req.body;
-  const user = getUserByEmail(email);
-  if (!email || !password || !user) {
-    return res.status(400).send("<h1>Login failed</h1>");
-  }
-  res.send("to-do");
-  /**
-   * [1] Compare submitted password to stored hash
-   * [2] If no match redirect back to same page so user can retry
-   * [3] If match create a session with their user ID,
-   *     set a cookie with the session ID,
-   *     redirect to the user's confession page (e.g. /confessions/3)
-   */
+async function post(req, res) {
+	const { email, password } = req.body
+	const user = await getUserByEmail(email)
+	if (!email || !password || !user) {
+		return res.status(400).send('<h1>Login failed</h1>')
+	}
+
+	/* [1] Compare submitted password to stored hash */
+	const result = await bcrypt.compare(password, user.hash)
+
+		/* [2] If no match redirect back to same page so user can retry */
+		if (!result) {
+      return res.status(400).send("<h1>Login failed</h1>")
+		} else {
+
+		/* [3] If match create a session with their user ID, */
+			const sessionID = await createSession(user.id)
+
+			/* set a cookie with the session ID, */
+			res.cookie('sid', sessionID, {
+				signed: true,
+				httpOnly: true,
+				maxAge: 6000,
+				sameSite: 'lax',
+			})
+			/* redirect to the user's confession page (e.g. /confessions/3) */
+			res.redirect(`/confessions/${user.id}`)
+		}
+	
 }
 
-module.exports = { get, post };
+module.exports = { get, post }
