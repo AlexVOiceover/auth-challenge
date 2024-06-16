b0c1d9070f0fcfaa047beb9d6eae302ee780bd54
Alexander <alex@alexvoiceover.com>
2024-06-16 21:25:32 +0100

    Challenge 02

--- src/routes/sign-up.js
+++ src/routes/sign-up.js
@@ -1,7 +1,9 @@
-const { Layout } = require("../templates.js");
+const { Layout } = require('../templates.js');
+const { createUser } = require('../model/user.js');
+const { createSession } = require('../model/session.js');
 
 function get(req, res) {
-  const title = "Create an account";
+  const title = 'Create an account';
   const content = /*html*/ `
     <div class="Cover">
       <h1>${title}</h1>
@@ -22,19 +24,37 @@ function get(req, res) {
   res.send(body);
 }
 
-function post(req, res) {
+async function post(req, res) {
+  const bcrypt = require('bcryptjs');
+
   const { email, password } = req.body;
   if (!email || !password) {
-    res.status(400).send("Bad input");
-  } else {
-    res.send("to-do");
-    /**
-     * [1] Hash the password
-     * [2] Create the user in the DB
-     * [3] Create the session with the new user's ID
-     * [4] Set a cookie with the session ID
-     * [5] Redirect to the user's confession page (e.g. /confessions/3)
-     */
+    return res.status(400).send('Bad input');
+  }
+
+  try {
+    /* [1] Hash the password */
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    /* [2] Create the user in the DB */
+    const user = await createUser(email, hashedPassword);
+
+    /* [3] Create the session with the new user's ID */
+    const sessionID = await createSession(user.id);
+
+    /* [4] Set a cookie with the session ID */
+    res.cookie('sid', sessionID, {
+      signed: true,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+      sameSite: 'lax',
+    });
+
+    /* [5] Redirect to the user's confession page (e.g. /confessions/3) */
+    res.redirect(`/confessions/${user.id}`);
+  } catch (error) {
+    console.error("Failed to create session:", error);
+    res.status(500).send('Internal server error');
   }
 }
 
